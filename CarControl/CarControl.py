8eb1c211e1cbc0b9366bf78dd8f180207e3cd194
snell245 <snell245@yahoo.com>
2018-07-17 15:56:16 -0600

    Should Be Fixed

--- CarControl/CarControl.py
+++ CarControl/CarControl.py
@@ -2,61 +2,62 @@ from pynput.keyboard import Key, Listener
 import l293d.driver as l293d
 import RPi.GPIO as GPIO
 
-# l293d.cleanup()
-# GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+# Drive Motor
+MotorA1 = 13
+MotorA2 = 11
+MotorA3 = 15
 
-# Motor 1 uses Pin 22, Pin 18, Pin 16
-motor1 = l293d.DC(22, 18, 16)
-# Motor 2 uses Pin 15, Pin 13, Pin 11
-motor2 = l293d.DC(15, 13, 11)
+# Steering Motor
+MotorB1 = 16
+MotorB2 = 18
+MotorB3 = 22
 
-# Run the motors so visible
-# for i in range(0,500):
-# motor1.clockwise()
-# motor2.clockwise()
+GPIO.setup(MotorA1, GPIO.OUT)
+GPIO.setup(MotorA2, GPIO.OUT)
+GPIO.setup(MotorA3, GPIO.OUT)
 
-# motor1.stop()
-# motor2.stop()
-# l293d.cleanup()
-# GPIO.cleanup()
+GPIO.setup(MotorB1, GPIO.OUT)
+GPIO.setup(MotorB2, GPIO.OUT)
+GPIO.setup(MotorB3, GPIO.OUT)
 
-# Line Drawn Here
-upButton = True
-downButton = True
-leftButton = True
-rightButton = True
+# p = GPIO.PWM(MotorA2, 0.5)
+# p.start(50)
+
 keepGoing = True
 
 while keepGoing:
 
     def press_down(key):
         if key == Key.up:
-            while upButton:
-                motor2.anticlockwise()
+            GPIO.output(MotorA1, GPIO.LOW)
+            GPIO.output(MotorA2, GPIO.HIGH)
+            GPIO.output(MotorA3, GPIO.HIGH)
         if key == Key.down:
-            while downButton:
-                motor2.clockwise()
+            GPIO.output(MotorA1, GPIO.HIGH)
+            GPIO.output(MotorA2, GPIO.LOW)
+            GPIO.output(MotorA3, GPIO.HIGH)
         if key == key.left:
-            while leftButton:
-                motor1.anticlockwise()
+            GPIO.output(MotorB1, GPIO.HIGH)
+            GPIO.output(MotorB2, GPIO.LOW)
+            GPIO.output(MotorB3, GPIO.HIGH)
+            # p.ChangeDutyCycle(100)
         if key == key.right:
-            while rightButton:
-                motor1.clockwise()
+            GPIO.output(MotorB1, GPIO.LOW)
+            GPIO.output(MotorB2, GPIO.HIGH)
+            GPIO.output(MotorB3, GPIO.HIGH)
+            # p.ChangeDutyCycle(50)
 
 
     def release(key):
         if key == Key.up:
-            upButton = False
-            # motor1.stop()
+            GPIO.output(MotorA3, GPIO.LOW)
         if key == Key.down:
-            downButton = False
-            # motor1.stop()
+            GPIO.output(MotorA3, GPIO.LOW)
         if key == Key.left:
-            leftButton = False
-            # motor2.stop()
+            GPIO.output(MotorB3, GPIO.LOW)
         if key == key.right:
-            rightButton = False
-            # motor2.stop()
+            GPIO.output(MotorB3, GPIO.LOW)
 
         if key == Key.esc:
             keepGoing = False
@@ -64,33 +65,8 @@ while keepGoing:
             GPIO.cleanup()
             return False
 
-with Listener(
-        on_press=press_down,
-        on_release=release) as listener:
-    listener.join()
 
-# import l293d
-
-# motor1 = l293d.DC(15,13,11)
-
-# motor1.clockwise()
-
-# l293d.cleanup()
-
-# from gpiozero import Motor
-# from time import sleep
-
-# motor1 = Motor(22,18,16)
-# motor2 = Motor(15,13,11)
-
-# motor1.forward()
-# sleep(2)
-# motor1.backward()
-# sleep(2)
-# motor1.stop()
-
-# motor2.forward()
-# sleep(2)
-# motor2.backward()
-# sleep(2)
-# motor2.stop()
+    with Listener(
+            on_press=press_down,
+            on_release=release) as listener:
+        listener.join()
